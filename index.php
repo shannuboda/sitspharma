cb8be8ba1860a9b713441d554acb186bbab81ca5
shannuboda <shanmukha.boda@gmail.com>
2024-06-04 13:02:34 +0530

    new'

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php include('includes/header.php'); ?>
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./images/SID BTECH.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./images/SID DIP.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./images/SID MBA.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="d" style="width:100%;background:red;height:2rem"></div>
+
+    <div class="intro container">
+        <div class="welcome">
+            <h2>Welcome To Here</h2>
+            <p>Ramaiah Institute of Nursing Education and Research (RINER) was established in 1987 by Late Founder
+                Chairman, Sri. M S Ramaiah, a philanthropist and a great visionary. With over 30 years of legacy, the
+                college offers various nursing programmes at the undergraduate and postgraduate levels.
+
+                A strong team of motivated faculty, passionate students and exceptional pedagogical approach coupled
+                with advanced facilities and infrastructure make Ramaiah Institute of Nursing Education and Research one
+                of the most acclaimed nursing education institution. The quality education offered at the college has
+                prepared over 2500 graduates who are well placed globally and are contributing the best of their
+                services.</p>
+        </div>
+        <div class="wel-img">
+            <img src="https://images.collegedunia.com/public/college_data/images/campusimage/1471603449sid.png" alt="">
+        </div>
+    </div>
+
+
+    <section
+        class="d-flex text-white elementor-section elementor-top-section elementor-element elementor-element-3f564f98 elementor-section-full_width elementor-section-height-default elementor-section-height-default"
+        data-id="3f564f98" data-element_type="section">
+
+        <div class="content-inner mission clearfix">
+            <div class="content">
+                <h2 class="title">
+                    <span>Mission</span>
+                </h2>
+                <div class="desc">To exemplify as a centre of academic excellence
+                    through quality education, practice
+                    and research for the preparation of nursing students to achieve
+                    global competence.</div>
+            </div>
+            <div class="button-action">
+                <a href="https://msriner.com/#" class="btn-theme btn-cta">
+                </a>
+
+            </div>
+        </div>
+
+        <div class="content-inner vision clearfix">
+            <div class="content">
+                <h2 class="title">
+                    <span>Vision</span>
+                </h2>
+                <div class="desc">To stand out as a college with social concern
+                    imparting value based education to the
+                    students to achieve academic excellence.</div>
+            </div>
+            <div class="button-action">
+                <a href="https://msriner.com/#" class="btn-theme btn-cta">
+                </a>
+
+            </div>
+        </div>
+
+    </section>
+
+    <section class="courses container">
+      <h1><u>COURSES OFFERED</u></h1>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col course">
+    <div class="card h-100">
+      <img src="https://msriner.com/wp-content/uploads/2021/01/4a9a2e47ead90b205b3c44ed64baae55.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">General Nursing & Midwifery</h5>
+          <div class="cc" style="display:flex;justify-content:space-between">
+            <p>
+              intake:60
+            </p>
+            <p>
+              Duration:3 years
+            </p>
+            <br>
+          
+          </div>
+          <button class="course-btn">Preview</button>
+      </div>
+    </div>
+  </div>
+  <div class="col course">
+    <div class="card h-100">
+      <img src="https://msriner.com/wp-content/uploads/2021/01/4a9a2e47ead90b205b3c44ed64baae55.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">General Nursing & Midwifery</h5>
+          <div class="cc" style="display:flex;justify-content:space-between">
+            <p>
+              intake:60
+            </p>
+            <p>
+              Duration:3 years
+            </p>
+            <br>
+          
+          </div>
+          <button class="course-btn">Preview</button>
+      </div>
+    </div>
+  </div>
+  <div class="col course">
+    <div class="card h-100">
+      <img src="https://msriner.com/wp-content/uploads/2021/01/4a9a2e47ead90b205b3c44ed64baae55.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">General Nursing & Midwifery</h5>
+          <div class="cc" style="display:flex;justify-content:space-between">
+            <p>
+              intake:60
+            </p>
+            <p>
+              Duration:3 years
+            </p>
+            <br>
+          
+          </div>
+          <button class="course-btn">Preview</button>
+      </div>
+    </div>
+  </div>
+  <div class="col course">
+    <div class="card h-100">
+      <img src="https://msriner.com/wp-content/uploads/2021/01/4a9a2e47ead90b205b3c44ed64baae55.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">General Nursing & Midwifery</h5>
+          <div class="cc" style="display:flex;justify-content:space-between">
+            <p>
+              intake:60
+            </p>
+            <p>
+              Duration:3 years
+            </p>
+            <br>
+          
+          </div>
+          <button class="course-btn">Preview</button>
+      </div>
+    </div>
+  </div>
+</div>
+    </section>
+
+      <?php include('includes/footer.php');?>
+
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
